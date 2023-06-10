dde8601949d38a5ac601ef4b2ebd293d11adffe6
C. W. Wright <lidar532@gmail.com>
2023-06-10 12:29:04 -0400

    first commit test.

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+import sys
+
+
+# In[ ]:
+
+
+for p in sys.path:
+  print(p)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import test.testlib
+
+
+# In[ ]:
+
+
+
+
