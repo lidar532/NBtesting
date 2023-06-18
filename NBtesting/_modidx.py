ccf4ea152d56bec9e1b15cad535f4032c8343301
C. W. Wright <lidar532@gmail.com>
2023-06-17 23:34:09 -0400

    changed over to nbdev

--- NBtesting/_modidx.py
+++ NBtesting/_modidx.py
@@ -5,4 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://lidar532.github.io',
                 'git_url': 'https://github.com/lidar532/NBtesting',
                 'lib_path': 'NBtesting'},
-  'syms': {'NBtesting.core': {'NBtesting.core.foo': ('core.html#foo', 'NBtesting/core.py')}}}
+  'syms': { 'NBtesting.testlib': { 'NBtesting.testlib.test_a': ('testlib.html#test_a', 'NBtesting/testlib.py'),
+                                   'NBtesting.testlib.test_b': ('testlib.html#test_b', 'NBtesting/testlib.py'),
+                                   'NBtesting.testlib.test_widget': ('testlib.html#test_widget', 'NBtesting/testlib.py')},
+            'NBtesting.testlib2': { 'NBtesting.testlib2.test_lib2_a': ('testlib2.html#test_lib2_a', 'NBtesting/testlib2.py'),
+                                    'NBtesting.testlib2.test_lib2_b': ('testlib2.html#test_lib2_b', 'NBtesting/testlib2.py')},
+            'NBtesting.widgets': {'NBtesting.widgets.FolderSelector': ('widgets.html#folderselector', 'NBtesting/widgets.py')}}}
